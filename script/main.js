9310ceeaf97fcd524b84d66031a48e2d1b27d82a
RubyScarf <91581311+RubyScarf@users.noreply.github.com>
2024-11-03 21:33:43 -0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -75,12 +75,12 @@ const animationTimeline = () => {
             y: 10
         },
     "-=1")
-    .from(".three", 0.7, {
+    .from(".three", 0.9, {
         opacity: 0,
         y: 10
     })
     .to(".three",
-        0.7,
+        0.9,
         {
             opacity: 0,
             y: 10
