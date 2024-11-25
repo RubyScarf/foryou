34eaa8b26b359819d2b98dac67111718c9543bcb
RubyScarf <91581311+RubyScarf@users.noreply.github.com>
2024-11-24 22:00:07 -0800

    Update script.js

--- quiz_1/Assets/script.js
+++ quiz_1/Assets/script.js
@@ -174,7 +174,9 @@ function createLayout(questionObj) {
 }
 
 /** if answer button is clicked, execute this function. Shows "Right" or "wrong" text, depending on answer */
-function checkAnswer(selectedIndex, correctIndex) {
+function checkAnswer(questionIndex, selectedAnswerIndex) {
+    // Subtract 1 from correctAnswer to adjust for 1-based indexing
+    const correctAnswerIndex = questionArray[questionIndex].correctAnswer - 1;
     let feedbackText = document.getElementById('right-wrong');
     let hrElem = document.getElementById("answer-bar");
 
